4b60857562862cc8318ac74732fa1526f6d059f4
RubenJTL <ruben@ravn.co>
2023-04-17 18:52:41 -0500

    Add initial project

---
+++ FlickrTvOS/FlickrTvOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrTvOSApp.swift
+//  FlickrTvOS
+//
+//  Created by Ruben Torres on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FlickrTvOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
